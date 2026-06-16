8eadea513d1218110170ab24d38f8c6d5cbf1d20
mg-diego <diego.mg.umh@gmail.com>
2026-06-16 22:21:19 +0200

    Init commit - Setup Astro project with Tailwind CSS and basic routing

---
+++ astro.config.mjs
@@ -0,0 +1,9 @@
+﻿import { defineConfig } from 'astro/config';
+
+import tailwind from '@astrojs/tailwind';
+
+export default defineConfig({
+  site: 'https://mg-diego.github.io',
+  base: '/dmg-photography',
+  integrations: [tailwind()],
+});
