a3394e34ef1e2258ea0848083de5a886e8b4eb96
mg-diego <diego.mg.umh@gmail.com>
2026-06-16 22:59:54 +0200

    Improve visuals

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,16 @@
-/** @type {import('tailwindcss').Config} */
+﻿/** @type {import('tailwindcss').Config} */
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			fontFamily: {
+				sans: ['Inter', 'sans-serif'],
+				serif: ['"Playfair Display"', 'serif'],
+			},
+			colors: {
+				dark: '#0a0a0a',
+			}
+		},
 	},
 	plugins: [],
 }
